33c16cb6628b0d3ff632f497ce1110e1777f4397
Stefan Butz <stefan260699@gmail.com>
2022-01-20 08:13:28 +0100

    read buffer partly

--- main.cpp
+++ main.cpp
@@ -17,24 +17,8 @@ int main()
 	signal(SIGINT, signal_callback_handler);
 
 	TraceReader reader = TraceReader(0x8);
-	uint64_t i = 0;
-	uint64_t last_num = 0;
+	uint64_t count = 0;
 
-	//current:
-	//status read (virt_to_phys 3 + status read 1) = 4
-	//record read (virt_to_phys 3 + record read 1) = 4
-	//8 reads for one record
-	//
-	//virt_to_phys reduce
-	//ideas:
-	//- cache status address
-	//- calc record address
-	//- cache pdir pages (more generic) [DONE]
-	//2 reads for one record
-	//
-	//multiple reads at once
-	//- always read whole buffer [DONE]
-	//- read required parts (2 reads or 1 scatter read) [DONE]
 	while (!aborted) {
 		std::pair<size_t,size_t> result = reader.get_new_records();
 		if (result.first == 0) {
@@ -44,8 +28,8 @@ int main()
 		else {
 		    if (result.second)
                 std::cout << "Lost Events: " << result.second << std::endl;
-            i += result.first;
-            std::cout << std::dec << "\r i=" << i << std::flush;
+            count += result.first;
+            std::cout << std::dec << "\r count=" << count << std::flush;
 		}
 		//TODO: write to file
 	}
